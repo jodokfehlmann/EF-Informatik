0a1407a9ee342f1224cbbc954ba67448055ba4f7
jodokfehlmann <jodok.fehlmann@gmail.com>
2022-09-09 08:47:15 +0200

    schnelleversion primezahl

--- aufgaben/aufgabe 4.1.py
+++ aufgaben/aufgabe 4.1.py
@@ -1,22 +1,18 @@
-from operator import truediv
 import time
 
 sttime=time.time()
-
 nmax=10000
 prime_zahl=[]
 prime_zahl.append(2)
 
 for i in range(3,nmax+1):
     isprime=True
-    j=0
-    while (j<len(prime_zahl) and isprime):
+    for j in range(len(prime_zahl)):
         if i % prime_zahl[j]==0:
             isprime=False
-        j=j+1
     if isprime:
         prime_zahl.append(i)
-        
+
 eltime = time.time()-sttime
 print(prime_zahl)
 print("Rechen-Zeit: " + str(round(eltime,4)) + "sec")
