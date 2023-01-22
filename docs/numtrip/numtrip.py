7fb6bd421c11befbdc1f2fab57e83bb9d30fb498
jodokfehlmann <jodok.fehlmann@gmail.com>
2023-01-22 11:56:34 +0100

    fehler zu 90% behoben nur di efunktion feld_löschen gibt noch probleme...

--- docs/numtrip/numtrip.py
+++ docs/numtrip/numtrip.py
@@ -1,5 +1,7 @@
 import random
 
+n=5
+
 zahlen_matrix = [
     [128, 128,  16,  8,  8],
     [  4,  32,   8,  2,  1],
@@ -41,10 +43,10 @@ def eingabe_ueberpruefen(x,y):
     try:
         x=int(x)
         y=int(y)
-        if (x>0 and x<=5 and y>0 and y<=5):
+        if (x>0 and x<=n and y>0 and y<=n):
             return True
         else:
-            print("Es müssen zahlen zwischen 1 und 5 sein.")
+            print("Es müssen zahlen zwischen 1 und " + str(n) + " sein.")
             return False
     except:
         print("Natürliche Zahlen du Depp!!!")
@@ -69,10 +71,10 @@ def feld_löschen(x,y):
     if zahlen_matrix[x-1][y]== wert:
         zahlen_matrix[x-1][y]=-1
         feld_löschen(x-1,y)
-    if x < 4 and zahlen_matrix[x+1][y]== wert:
+    if x < n and zahlen_matrix[x+1][y]== wert:
         zahlen_matrix[x+1][y]=-1
         feld_löschen(x+1,y)
-    if y <4 and zahlen_matrix[x][y+1]== wert:
+    if y < n and zahlen_matrix[x][y+1]== wert:
         zahlen_matrix[x][y+1]=-1
         feld_löschen(x,y)
     if zahlen_matrix[x][y-1]== wert:
@@ -80,15 +82,15 @@ def feld_löschen(x,y):
         feld_löschen(x,y-1)
 
 def spielbar():
-    for a in range(5):
-        for b in range(5):
+    for a in range(n):
+        for b in range(n):
             if zahlen_matrix[a][b] == -1:
                 return True
 
             check_left = a > 0 and zahlen_matrix[a][b] == zahlen_matrix[a-1][b]
-            check_right = a < 4 and zahlen_matrix[a][b] == zahlen_matrix[a+1][b]
+            check_right = a < n and zahlen_matrix[a][b] == zahlen_matrix[a+1][b]
             check_up = b > 0 and zahlen_matrix[a][b] == zahlen_matrix[a][b-1]
-            check_down = b < 4 and zahlen_matrix[a][b] == zahlen_matrix[a][b+1]
+            check_down = b < n and zahlen_matrix[a][b] == zahlen_matrix[a][b+1]
 
             if check_left or check_right or check_up or check_down:
                 return True
@@ -97,8 +99,8 @@ def spielbar():
 
     
 def feld_auffüllen():
-    for j in range(5):
-        for i in range (4,0,-1):
+    for j in range(n):
+        for i in range (n-1,0,-1):
             if zahlen_matrix[i][j] == -1:
                 ip=i
                 weitersuchen=True
@@ -121,21 +123,21 @@ def feld_auffüllen():
 def feldzeichnen():
     obere_zeile()
     rand()
-    for i in range(5):
+    for i in range(n):
         leer_zeile()
         dazwischen_zahl(zahlen_matrix[i],i+1)
         leer_zeile()
         rand()
 
-def ist_loesachbar(zahlen_matrix, x, y):
+def ist_loesachbar(x, y):
     # check_up
     if x > 0 and zahlen_matrix[x-1][y] == zahlen_matrix[x][y]:
         return True
     # check_down
-    if x < 4 and zahlen_matrix[x+1][y] == zahlen_matrix[x][y]:
+    if x < n and zahlen_matrix[x+1][y] == zahlen_matrix[x][y]:
         return True
     # check_left
-    if y < -1 and zahlen_matrix[x][y+1] == zahlen_matrix[x][y]:
+    if y < n and zahlen_matrix[x][y+1] == zahlen_matrix[x][y]:
         return True
     # ckeck_right
     if y > 0 and zahlen_matrix[x][y-1] == zahlen_matrix[x][y]:
@@ -145,33 +147,35 @@ def ist_loesachbar(zahlen_matrix, x, y):
 
 
 zahlen_matrix = []
-for i in range(5):
+for i in range(n):
     neuezeile=[]
-    for j in range(5):
+    for j in range(n):
         neuezeile.append(2**(int(random.random()*6)))
     zahlen_matrix.append(neuezeile)
 
         
 while True:
+    feldzeichnen()
     x, y=eingabe()
     global wert
     x -= 1
     y -= 1
-    wert = zahlen_matrix[x][y]
-    if wert*2 >= 2048:
-        print("Du hast Gewonnen!!!")
-    else: 
-        zahlen_matrix[x][y]= wert*2
 
-    if ist_loesachbar(zahlen_matrix, x, y):
+    if ist_loesachbar(x, y):
         feld_löschen(x,y)
         	
     else:
         print("Du kannst dieses feld nicht löschen")
         eingabe()
     
+    wert = zahlen_matrix[x][y]
+    if wert*2 >= 2048:
+        print("Du hast Gewonnen!!!")
+    else: 
+        zahlen_matrix[x][y]= wert*2
+
     # print(zahlen_matrix)
     feld_auffüllen()
-    feldzeichnen()
+    
     if not spielbar():
         print("Du hast verlorem")
